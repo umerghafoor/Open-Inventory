45b5d376f99a7bea09cda6fe060afc8b1025a89a
Asad Abdullah <132757067+Asad1Abdullah@users.noreply.github.com>
2023-05-12 20:13:32 +0500

    Just testing commit

--- InventrySysytem/Header.h
+++ InventrySysytem/Header.h
@@ -1,1 +1,59 @@
+#pragma once
 #include<iostream>
+#include<string>
+class user
+{
+private:
+	string name;
+	string email;
+	string username;
+	string password;
+public:
+	user()
+	{
+		name = "0/";
+		email = "0/";
+		username = "0/";
+		password = "0/";
+	}
+	user(string n, string e, string u, string p)
+	{
+		this->name = n;
+		this->email = e;
+		this->username = u;
+		this->password = p;
+	}
+	void setname(string name)
+	{
+		this->name = name;
+	}
+	void setname(string email)
+	{
+		this->email = email;
+	}
+	void setname(string usernname)
+	{
+		this->username = username;
+	}
+	void setname(string password)
+	{
+		this->password = password;
+	}
+
+	string getname()
+	{
+		return name;
+	}
+	string getemail()
+	{
+		return email;
+	}
+	string getusername()
+	{
+		return username;
+	}
+	string getpassword()
+	{
+		return password;
+	}
+};
--- InventrySysytem/Source.cpp
+++ InventrySysytem/Source.cpp
@@ -1,9 +1,9 @@
 #include "Header.h"
-
+#include<iostream>
+using namespace std;
 int main()
 {
 	std::cout << "Changes";
 	std::cout << "Hello world!" << std::endl;
 	return 0;
-
 }
