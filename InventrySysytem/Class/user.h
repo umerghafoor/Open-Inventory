c83a68e68a977dd519463829079cde1466a4cd32
umerghafoor <umerghaforr@gmail.com>
2023-05-13 13:29:20 +0500

    update

--- InventrySysytem/Class/user.h
+++ InventrySysytem/Class/user.h
@@ -7,6 +7,14 @@ protected:
 	std::string name;
 	std::string email;
 	std::string password;
-public:
-	bool checkID();
+	bool checkID(std::string, std::string);
+
 };
+
+bool user::checkID(std::string password, std::string newpassword)
+{
+	if (password == newpassword)
+		return true;
+	else
+		return false;
+}
--- InventrySysytem/customermenucpp.cpp
+++ InventrySysytem/customermenucpp.cpp
@@ -3,14 +3,20 @@
 
 void CustomerMenu()
 {
-	/*Customer c1(11,"Asad","asadabdullah@gmail.com","asad123");*/
+//	Customer c1(2,"user2","user1@gmail.com","12345678");
 	/*yaha pe sign up ka poochna ha*/
-	Customer c2(11);
+	Customer c2(2,"12345678");
 	std::cout << c2.getName();
+}
+
+void ItemFunctions()
+{
+	inventory i1;
+	std::cout << i1.addItem(2, "item2", 40.0, 99);
 }
 
 int main()
 {
-	CustomerMenu();
+	ItemFunctions();
 	return 0;
 }
