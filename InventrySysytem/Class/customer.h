bdb762f52f2d5fc9f00a347aa6359193009bcd8b
umerghafoor <umerghaforr@gmail.com>
2023-05-13 16:59:40 +0500

    customer Functionalities
    All function related to customer has bee created

--- InventrySysytem/Class/customer.h
+++ InventrySysytem/Class/customer.h
@@ -5,25 +5,44 @@
 #include<fstream>
 #include <sstream>
 #include <vector>
+#include <ctime>
+
 #include"Inventory.h"
 #include"user.h"
 
-class Customer:private user
+class Customer :private user, private inventory
 {	
 	std::vector<Item> cart;
 public:
 	Customer(int,std::string);
 	Customer(int _ID, std::string, std::string, std::string);
+
+	//getter
 	int getID();
 	std::string getName();
 	std::string getEmail();
+
+	void displayAll();
+
+	//Cart managment
+	void displayCart();
+	bool addToCartbyNo(int,int);
+	bool addToCartbyName(std::string, int);
+	bool removeFromCart(int);
+	bool editQuantity(int, int);
+
+	//Billing
+	float totalAmount();
+	bool doneShoping(float,float,float);
+
+	void logOut();
 };
 
 Customer::Customer(int _ID , std::string password)
 {
 	this->ID = _ID;
 
-	bool customerFound = false;
+	logedIn = false;
 	std::ifstream customerDataBase("customerDataBase.csv");
 	std::string _id, _name, _email, _password;
 	std::string line;
@@ -42,12 +61,12 @@ Customer::Customer(int _ID , std::string password)
 			name = _name;
 			email = _email;
 			password = _password;
-			customerFound = true;
+			logedIn = true;
 			break;
 		}
 	}
 	customerDataBase.close();
-	if (!customerFound) 
+	if (!logedIn)
 	{
 		ID = -1;
 		name = '\0';
@@ -87,6 +106,7 @@ Customer::Customer(int _ID, std::string _name, std::string _email, std::string _
 		customerDataBase << ID << "," << name << "," << email << "," << password << "\n";
 		customerDataBase.close();
 	}
+	logedIn = false;
 }
 
 int Customer::getID()
@@ -101,3 +121,139 @@ std::string Customer::getEmail()
 {
 	return email;
 }
+
+float Customer::totalAmount()
+{
+	float total = 0;
+	if (logedIn)
+	{
+		for (auto i = 0; i != cart.size(); i++)
+		{
+			total += cart[i].price * cart[i].quantity;
+		}
+		return total;
+	}
+	else
+	{
+		return 0;
+	}
+}
+bool Customer::doneShoping(float cash, float change, float received)
+{
+	if (logedIn)
+	{
+		std::ofstream logFile("log.csv", std::ios::app);
+
+		//Boilerplate code for ctime library
+		std::time_t t = std::time(0);
+		std::tm now;
+		localtime_s(&now, &t);
+
+		logFile << received << "," << cash << "," << change << "," << ID << "," << name << ","
+			<< now.tm_mday << '/' << now.tm_mon + 1 << '/' << now.tm_year + 1900 << ' ' << now.tm_hour << ':' << now.tm_min
+			<< std::endl;
+		
+		logFile.close();
+	}
+	else
+		return true;
+}
+void Customer::logOut()
+{
+	logedIn = false;
+}
+
+bool Customer::addToCartbyNo(int itemNo,int quantity)
+{
+	if (logedIn)
+	{
+		Item item = ReturnItemByNo(itemNo);
+		item.quantity = quantity;
+		cart.push_back(item);
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+bool Customer::addToCartbyName(std::string itemName, int quantity)
+{
+	if (logedIn)
+	{
+		Item item = ReturnItemByName(itemName);
+		item.quantity = quantity;
+		cart.push_back(item);
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+bool Customer::removeFromCart(int itemNo)
+{
+	if (logedIn)
+	{
+		if (itemNo < cart.size())
+		{
+			cart.erase(cart.begin() + itemNo);
+			return true;
+		}
+		else
+			return false;
+	}
+	else
+		return false;
+}
+bool Customer::editQuantity(int itemNo, int newQuantity)
+{
+	if (logedIn)
+	{
+		if (itemNo < cart.size())
+		{
+			cart[itemNo].quantity = newQuantity;
+			return true;
+		}
+		else
+			return false;
+	}
+	else
+		return false;
+}
+
+void Customer::displayAll()
+{
+	if (logedIn)
+	{
+		std::vector<Item> dispalyItems = ReturnAllItems();
+		for (auto i = 0; i != dispalyItems.size(); i++)
+		{
+			std::cout << dispalyItems[i].itemNo << '\t' << dispalyItems[i].itemName << '\t' << dispalyItems[i].price << std::endl;
+		}
+	}
+	else
+	{
+		std::cout << "Please Login\n";
+	}
+}
+void Customer::displayCart()
+{
+	float total = 0;
+	if (logedIn)
+	{
+		std::cout << "No." << '\t' << "Code" << '\t' << "Name"
+			<< '\t' << "Price" << '\t' << "Quant" << '\t' << "Total" << std::endl;
+		for (auto i = 0; i != cart.size(); i++)
+		{
+			total = cart[i].price * cart[i].quantity;
+			std::cout << i + 1 << '\t' << cart[i].itemNo << '\t' << cart[i].itemName
+				<< '\t' << cart[i].price << '\t' << cart[i].quantity << '\t' << total << std::endl;
+		}
+	}
+	else
+	{
+		std::cout << "Please Login\n";
+	}
+}
+
--- InventrySysytem/Class/inventory.h
+++ InventrySysytem/Class/inventory.h
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <vector>
 
 struct Item
 {
@@ -13,22 +14,34 @@ struct Item
         price = 0;
         quantity = 0;
     }
+    Item(int itemNo, std::string itemName, float price, int quantity)
+    {
+        this->itemNo = itemNo;
+        this->itemName = itemName;
+        this->price = price;
+        this->quantity = quantity;
+    }
 };
 
 class inventory
 {
     Item item;
-public:
+    
+protected:
     inventory() {}
-    //venderor will use this to add items
+    //venderor will use this to add new items
     bool addItem(int, std::string, float, int);
 
     //it will return item no one will be able to modify it in data base
-    Item SearchByNo(int);
+    Item ReturnItemByNo(int);
+    Item ReturnItemByName(std::string);
+    std::vector<Item> ReturnAllItems();
 
     //user will use this
-    bool buyItem(int);
+    bool buyItem(int, int);
 
+    // use to edit quantity
+    bool editItemQuantity(int, int);
 };
 
 bool inventory::addItem(int itemNo, std::string itemName, float price, int Quantity)
@@ -55,7 +68,8 @@ bool inventory::addItem(int itemNo, std::string itemName, float price, int Quant
     itemDataBaseOut.close();
     return true;
 }
-Item inventory::SearchByNo(int itemNo)
+
+Item inventory::ReturnItemByNo(int itemNo)
 {
     std::ifstream itemDataBase("ItemDataBase.csv");
     std::string line;
@@ -77,11 +91,56 @@ Item inventory::SearchByNo(int itemNo)
         }
     }
 }
-bool inventory::buyItem(int itemNo)
+Item inventory::ReturnItemByName(std::string itemName)
 {
     std::ifstream itemDataBase("ItemDataBase.csv");
     std::string line;
     while (std::getline(itemDataBase, line)) {
+        std::stringstream ss(line);
+        std::string _itemNo, _itemName, _price, _quantity;
+        std::getline(ss, _itemNo, ',');
+        std::getline(ss, _itemName, ',');
+        std::getline(ss, _price, ',');
+        std::getline(ss, _quantity, ',');
+        if (itemName == _itemName)
+        {
+            item.itemNo = (std::stoi(_itemNo));
+            item.itemName = _itemName;
+            item.price = std::stoi(_price);
+            item.quantity = std::stoi(_quantity);
+            return item;
+        }
+    }
+}
+std::vector<Item> inventory::ReturnAllItems()
+{
+    std::vector<Item> allItems;
+    std::ifstream itemDataBase("ItemDataBase.csv");
+    std::string line;
+    while (std::getline(itemDataBase, line)) 
+    {
+        std::stringstream ss(line);
+        std::string _itemNo, _itemName, _price, _quantity;
+        std::getline(ss, _itemNo, ',');
+        std::getline(ss, _itemName, ',');
+        std::getline(ss, _price, ',');
+        std::getline(ss, _quantity, ',');
+        int itemNo = std::stoi(_itemNo);
+        float price = std::stof(_price);
+        int quantity = std::stoi(_quantity);
+        Item item(itemNo, _itemName, price, quantity);
+        allItems.push_back(item);
+    }
+    itemDataBase.close();
+    return allItems;
+}
+
+bool inventory::buyItem(int itemNo,int quantity)
+{
+    std::ifstream itemDataBase("ItemDataBase.csv");
+    std::string line;
+    while (std::getline(itemDataBase, line)) 
+    {
         std::stringstream ss(line);
         std::string _itemNo, _itemName, _price, _quantity;
         std::getline(ss, _itemNo, ',');
@@ -98,4 +157,42 @@ bool inventory::buyItem(int itemNo)
             return true;
         }
     }
+}
+
+/*
+private memeber functions
+*/
+bool inventory::editItemQuantity(int itemNo, int newQuantity)
+{
+    std::ifstream itemDataBase("ItemDataBase.csv");
+    std::ofstream tempDataBase("temp.csv");
+
+    std::string line;
+    while (std::getline(itemDataBase, line))
+    {
+        std::stringstream ss(line);
+        std::string _itemNo, _itemName, _price, _quantity;
+        std::getline(ss, _itemNo, ',');
+        std::getline(ss, _itemName, ',');
+        std::getline(ss, _price, ',');
+        std::getline(ss, _quantity, ',');
+        int dbItemNo = std::stoi(_itemNo);
+
+        if (dbItemNo == itemNo)
+        {
+            int dbQuantity = std::stoi(_quantity);
+            dbQuantity += newQuantity;
+            tempDataBase << dbItemNo << "," << _itemName << "," << _price << "," << dbQuantity << "\n";
+        }
+        else
+        {
+            tempDataBase << line << "\n";
+        }
+    }
+    itemDataBase.close();
+    tempDataBase.close();
+
+    std::remove("ItemDataBase.csv");
+    std::rename("temp.csv", "ItemDataBase.csv");
+    return true;
 }
